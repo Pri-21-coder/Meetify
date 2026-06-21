7215052ed103fb1d3fdb497f90f41aa3d7a11632
Pri-21-coder <prithapal114@gmail.com>
2026-06-21 11:51:05 +0530

    Setting initial Backend Structure with mongoDb database

---
+++ backend/src/app.js
@@ -0,0 +1,27 @@
+import "dotenv/config"; // import because password is in env
+import express from "express";
+import {createServer} from "node:http";
+
+import {connectToSocket} from "./controllers/socketManager.js";
+import mongoose from "mongoose";
+import cors from "cors";
+
+const app = express();
+const server = createServer(app);
+const io = connectToSocket(server)
+
+app.set("port", (process.env.PORT || 8000))
+app.use(cors());
+app.use(express.json({limit: "40kb"}));
+app.use(express.urlencoded({limit: "40kb", extended: true}));
+app.get("/home", (req, res)=>{
+    return res.json({"hello": "world"})
+});
+const start = async () => {
+    const connectionDb = await mongoose.connect(`mongodb+srv://prithapal114_db_user:${process.env.PASSWORD}@cluster0.smypbll.mongodb.net/`)
+    console.log(`Mongo Connected DB Host: ${connectionDb.connection.host}`)
+    server.listen(app.get("port"),()=>{
+        console.log("Listen on port 8000")
+    });
+}
+start();
