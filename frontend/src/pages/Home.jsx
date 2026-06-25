9af926a93bf7a5dbd34983b06ef47147114f39c0
Pri-21-coder <prithapal114@gmail.com>
2026-06-25 13:54:48 +0530

    Create Home Component and added backend logic to track History

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,53 @@
-import React from "react";
+import React, { useContext, useState } from 'react'
 import withAuth from "../utlis/withAuth";
+import { useNavigate } from "react-router-dom";
+import "../App.css";
+import { Button, IconButton, TextField } from '@mui/material';
+import RestoreIcon from '@mui/icons-material/Restore';
+import { AuthContext } from "../contexts/AuthContext";
 //this component not accesible by everyone Authenticate user can access this
 function HomeComponent(){
+    let navigate = useNavigate();
+    const [meetingCode, setMeetingCode] = useState("");
+    const {addToUserHistory} = useContext(AuthContext);
+    let handleJoinVideoCall = async ()=> {
+        await addToUserHistory(meetingCode);
+        navigate(`/${meetingCode}`)
+    }
     return (
-        <div>HomeComponent</div>
+        <>
+        <div className="navBar">
+            <div style={{display: "flex",alignItems: "center"}}>
+                <h2>Meetify</h2>
+            </div>
+            <div style={{display: "flex", alignItem: "center"}}>
+                <IconButton>
+                    <RestoreIcon/>
+                </IconButton>
+                <p>History</p>
+                <Button onClick={()=>{
+                    localStorage.removeItem("token")
+                    navigate("/auth")
+                }}>
+                    Logout
+                </Button>
+            </div>
+        </div>
+        <div className="meetContainer">
+            <div className="leftPanel">
+                <div>
+                    <h2>Providing Quality Video Call Just Like Quality Education</h2>
+                    <div style={{display: 'flex', gap: "10px"}}>
+                        <TextField onChange={e => setMeetingCode(e.target.value)} id="outlined-basic" label="Meeting Code" variant="outlined"/>
+                        <Button onClick={handleJoinVideoCall} variant='contained'>Join</Button>
+                    </div>
+                </div>
+            </div>
+            <div className="rightPanel">
+                <img srcSet="/logo1.png" alt="" />
+            </div>
+        </div>
+        </>
     )
 }
 
